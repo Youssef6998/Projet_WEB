adbe236377cb30d43f1605da4d8fa56092d5586c
NathanVLC <nathan.vella45@gmail.com>
2026-03-13 11:58:55 +0100

    changement barre nav, rajout des pages mentions légales et a propos de nous, rajout du début de la page entreprises

--- index.php
+++ index.php
@@ -10,20 +10,29 @@ $loader = new \Twig\Loader\FilesystemLoader('templates');
 $twig = new \Twig\Environment($loader, ['debug' => true]);
 
 $model = new StageModel();
-$page = max(1, (int)($_GET['page'] ?? 1));
 
-$uri = $_GET['uri'] ?? 'stages';
+
+$page = (int)($_GET['page'] ?? 1);
+
+$paginationInfo = $model->getPaginatedStages(1);  
+$totalPages = $paginationInfo['totalPages'];
+$page = max(1, min($page, $totalPages));
+
+$uri = $_GET['uri'] ?? 'cherche-stage';
 $pageTemplate = match($uri) {
     'cherche-stage' => 'cherche_stage.twig.html',
     'stages' => 'stages.twig.html',
     'home' => 'cherche_stage.twig.html',
     'login' => 'connexion.twig.html',
     'register' => 'inscription.twig.html',
-    default => '404.twig.html'
+    'mentions' => 'mentions.twig.html',
+    'entreprises' => 'cherche_entreprises.twig.html',
+    'nous' => 'nous.twig.html',
+    default => '404.twig.html',
 };
 
 if ($uri === 'stages' || $uri === 'cherche-stage') {
-    $data = $model->getPaginatedStages($page);
+    $data = $model->getPaginatedStages($page);  
     $data['uri'] = $uri;
     $data['domaine'] = $_GET['domaine'] ?? '';
 } else {
